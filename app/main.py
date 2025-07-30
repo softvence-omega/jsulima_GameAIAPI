534e7adaf4d7a9119563a92bd4561f353726166b
marzia <marziasultanar01@gmail.com>
2025-07-30 06:38:16 +0000

    add base url

--- app/main.py
+++ app/main.py
@@ -41,6 +41,9 @@ app.include_router(nfl_lineup_prediction.router, prefix="/predict/nfl", tags=["N
 app.include_router(nfl_head_to_head.router, prefix="/predict/nfl", tags=["NFL"])
 app.include_router(nfl_win_percentage_endpoint.router, prefix="/predict/nfl", tags=["NFL"])
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the GameAPI! Use the /docs endpoint to explore available endpoints."}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
